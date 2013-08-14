b66d4f1c79776d9ff2112cda853e140063b518c9
Matthew Bernhardt <bernhardt.7@osu.edu>
2013-08-14 00:08:30 -0400

    Added announcement about opponents browse, and switched front-page announcements to a better/semantic format

--- index.php
+++ index.php
@@ -4,8 +4,24 @@
 	include "includes/block_header.php";
 ?>
 		<h1>Welcome to Massive Report Data!</h1>
+		<div class="column">
+
+		</div>
+		<div class="">
+
+		</div>
 		<p>This is a repository for data concerning the Columbus Crew - its history and its context. I analyze and visualize a wide variety of information in order to help fans of the team to understand its performance.</p>
 		<p>This site launched in late 2012; it is still being built, so I invite you to take a look around, and let me know if there is something I've overlooked or gotten wrong. My email address is at the bottom of every page.</p>
+		<div class="update">
+			<h2><a href="/browse/opponents">All-time Opponents</a></h2>
+			<p>August 13<br>
+			This update was actually posted some time ago, but the navigation changes needed to expose it haven't happened yet. This is a list of all opponents that the Crew have faced in official competitions. Can't remember if the Crew ever played a team from Syracuse (<a href="/opponent/121">yes, they have</a>)? This is your answer.</p>
+		</div>
+		<div class="update">
+			<h2><a href="/visualize/combinations">Player Combinations</a></h2>
+			<p>July 14<br>
+			I'm happy to release an exploration tool for player combinations. This project allows people to explore how the Crew's performance has changed as different combinations of players have changed.</p>
+		</div>
 		<p><em>Matthew Bernhardt</em><br />
 		Historian and Statistician<br />
 		Massive Report</p>
