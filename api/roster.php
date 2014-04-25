b7730ef41afc0af05c932b50ab4b30fc9e006282
Matthew Bernhardt <mjbernha@mit.edu>
2014-04-25 07:59:20 -0400

    Implementing an updateable roster visualization
    driven by the new API

--- api/roster.php
+++ api/roster.php
@@ -44,7 +44,7 @@ class Roster extends Base
 		if($this->teamID > 0) {
 			$strTeamClause = "TeamID = " . $this->teamID . " AND ";
 		} else {
-			$strTeamClause = "";
+			$strTeamClause = "TeamID != 17 AND ";
 		}
 		$sql = "SELECT p.ID AS PlayerID, CONCAT(p.FirstName,' ',p.LastName) AS PlayerName, p.Position, p.RosterNumber, p.Height_Feet, p.Height_Inches, p.Birthplace, p.Citizenship, p.Weight, p.DOB, m.TeamID, SUM(IF(m.TimeOff>0,1,0)) AS GP, SUM(IF(m.TimeOff>0,IF(m.TimeOn=0,1,0),0)) AS GS, SUM(TimeOff-TimeOn) AS Minutes, SUM(Ejected) AS Ejections, MAX(IF(m.TimeOff>0,MatchTime,0)) AS LastPlayed "
 		."FROM tbl_gameminutes m "
@@ -52,7 +52,7 @@ class Roster extends Base
 		."INNER JOIN tbl_players p ON m.PlayerID = p.ID "
 		."WHERE " . $strTeamClause . " YEAR(MatchTime) = " . $this->year . " "
 		."GROUP BY m.PlayerID, m.TeamID "
-		."ORDER BY LastName, FirstName";
+		."ORDER BY Weight DESC, Minutes DESC, LastName, FirstName";
 	
 		$rs = mysqli_query($this->connection, $sql) or die(mysqli_error($this->connection));
 
--- javascript/visualize_roster.js
+++ javascript/visualize_roster.js
@@ -1,5 +1,4 @@
 <script type="text/javascript">
-	var thisTeam = 0;
 	
 	var w = $("#chart").width();
 	var h = w * (3/4);
@@ -21,83 +20,105 @@
 		.domain([81,60])
 		.range([margin.top,h-margin.bottom]);
 
+	// chart
 	var vis = d3.select("#chart")
 		.append("svg:svg")
 		.attr("width",w)
 		.attr("height",h);
 
-	var foo = d3.json("/api/roster.php?term="+thisTeam, function(error, json) {
-		if(error) {
-			console.log(error);
-		}
+	// X axis
+	var xAxis = d3.svg.axis()
+		.scale(weightScale)
+		.tickSize((-1*h)+margin.top+margin.bottom)
+		.tickFormat(function(d) { return d+" lb"})
+		.orient('bottom');
 
-		var maxMinutes = d3.max(json, function(d) { return +d.Minutes; } );
+	var xAxisElem = vis.append('g')
+		.attr('class', 'x axis')
+		.attr('transform','translate(0,' + (h - margin.bottom ) + ')')
+		.call(xAxis);
 
-		var timeScale = d3.scale.linear()
-			.domain([0,maxMinutes])
-			.range([2,10]);
+	xAxisElem.selectAll("text").attr('y','10');
 
-		var xAxis = d3.svg.axis()
-			.scale(weightScale)
-			.tickSize((-1*h)+margin.top+margin.bottom)
-			.tickFormat(function(d) { return d+" lb"})
-			.orient('bottom');
+	// Y axis
+	var yAxis = d3.svg.axis()
+		.scale(heightScale)
+		.tickSize((-1*w)+margin.left+margin.right)
+		.tickValues([60,63,66,69,72,75,78,81])
+		.tickFormat(function(d) { return parseInt(d / 12)+"'-"+parseInt(d % 12)+'" '; })
+		.orient('left');
 
-		var xAxisElem = vis.append('g')
-			.attr('class', 'x axis')
-			.attr('transform','translate(0,' + (h - margin.bottom ) + ')')
-			.call(xAxis);
+	var yAxisElem = vis.append('g')
+		.attr('class','y axis')
+		.attr('transform','translate(' + margin.left + ',0)')
+		.call(yAxis);
 
-		xAxisElem.selectAll("text").attr('y','10');
+	yAxisElem.selectAll("text").attr('x','-6');
 
-		var yAxis = d3.svg.axis()
-			.scale(heightScale)
-			.tickSize((-1*w)+margin.left+margin.right)
-			.tickValues([60,63,66,69,72,75,78,81])
-			.tickFormat(function(d) { return parseInt(d / 12)+"'-"+parseInt(d % 12)+'" '; })
-			.orient('left');
+	// All player container
+	var ac = vis.append("g").attr("class","ac");
 
-		var yAxisElem = vis.append('g')
-			.attr('class','y axis')
-			.attr('transform','translate(' + margin.left + ',0)')
-			.call(yAxis);
+	// Player container
+	var pc = vis.append("g").attr("class","pc");
 
-		yAxisElem.selectAll("text").attr('x','-6');
+	function chartPlayers(thisTeam,thisContainer,thisColor,showLabels) {
 
-		// Players container
-		var players = vis.append("g").attr("class","players");
+		d3.json("/api/roster.php?term="+thisTeam, function(error,json) {
+			if(error) {
+				console.log(error);
+			}
 
-		var node = players.selectAll("g")
-			.data(json)
-			.enter()
-			.append("g")
-			.attr("class","player")
-			.attr("data-playerID",function(d) {return d.PlayerID});
+			// Get maximum minutes played, and construct time scale
+			var maxMinutes = d3.max(json, function(d) { return +d.Minutes; } );
+			var timeScale = d3.scale.linear()
+				.domain([0,maxMinutes])
+				.range([5,15]);
 
-		// player circle
-		node.append("circle")
-			.attr("cx",function(d) {return weightScale(parseInt(d.Weight))})
-			.attr("cy",function(d) {return heightScale(parseInt(d.Height_Inches)+12*parseInt(d.Height_Feet))})
-			.attr("r",function(d) {return timeScale(parseInt(d.Minutes))})
-			.attr("class",function(d) {return d.Position})
-			.attr("fill","#404040")
-			.attr("stroke","white")
-			.attr("stroke-width","2")
-			.attr("opacity","0.5");
+			// Individual player containers
+			var player = thisContainer.selectAll("g")
+				.data(json,function(d) { return d.PlayerID});
 
-		node.append("text")
-			.attr("dx", 12)
-			.attr("dy", "0.25em")
-			.attr("x",function(d) {return weightScale(parseInt(d.Weight))})
-			.attr("y",function(d) {return heightScale(parseInt(d.Height_Inches)+12*parseInt(d.Height_Feet))})
-			.text(function(d) { return d.PlayerName });
-	});
+			// Adding new players
+			var newPlayer = player
+				.enter()
+				.append("g")
+				.attr("class","player")
+				.attr("data-id",function(d) { return d.PlayerID });
+
+			// player circle
+			player.append("circle")
+				.attr("cx",function(d) {return weightScale(parseInt(d.Weight))})
+				.attr("cy",function(d) {return heightScale(parseInt(d.Height_Inches)+12*parseInt(d.Height_Feet))})
+				.attr("r",function(d) {return timeScale(parseInt(d.Minutes))})
+				.attr("class",function(d) {return showLabels===true ? d.Position : ""})
+				.attr("fill",thisColor)
+				.attr("stroke",function(d) {return showLabels===true ? "red" : "#000"})
+				.attr("stroke-width",function(d) {return showLabels===true ? "2" : "0"})
+				.attr("opacity","1");
+
+			if(showLabels===true) {
+				player.append("text")
+					.attr("dx", 20)
+					.attr("dy", "0.25em")
+					.attr("x",function(d) {return weightScale(parseInt(d.Weight))})
+					.attr("y",function(d) {return heightScale(parseInt(d.Height_Inches)+12*parseInt(d.Height_Feet))})
+					.text(function(d) { return d.PlayerName });
+			}
+
+			player.exit().remove();
+
+		});
+
+	};
+
+	// Graph all players for context
+	chartPlayers(0,ac,"#202020",false);
 
 	// This is the update code
 	$("#filter label").click(function(d) {
-		var thisTeam = $(this).prev("input").attr("data-id");
-		console.log('Clicked on team '+thisTeam);
+		var reqTeam = $(this).prev("input").attr("data-id");
 		$("#filter label").removeClass("selected");
+		chartPlayers(reqTeam,pc,"#666",true);
 		$(this).addClass("selected");
 		
 	});
@@ -125,18 +146,23 @@
 }
 .player:hover text {
 	display: inherit;
-	fill: #d9ad5b;
+	fill: #e3c803;
+	font-weight: bold;
 }
 	circle.Goalkeeper {
 		stroke: #F7E7D4;
+		fill: #F7E7D4;
 	}
 	circle.Defender {
-		stroke: #E7CB96;		
+		stroke: #E7CB96;
+		fill: #E7CB96;	
 	}
 	circle.Midfielder {
 		stroke: #A69688;
+		fill: #A69688;
 	}
 	circle.Forward {
 		stroke: #C69380;	
+		fill: #C69380;
 	}
 </style>
