a9cfcaa02ba4508971797a55755d3587c2597463
Matt Bernhardt <mjbernha@mit.edu>
2015-07-02 22:44:07 -0400

    Adds GitHub to footer

--- includes/block_footer.php
+++ includes/block_footer.php
@@ -33,6 +33,7 @@
 	    <ul class="inline" style="float: right;">
 	      <li><a href="/blog">Blog</a></li>
 	      <li><a href="/about">About</a></li>
+	      <li><a href="https://github.com/matt-bernhardt/massive-report-data">GitHub</a></li>
 	      <li style="margin-right: 30px;"><a href="http://flattr.com/thing/1801973/Massive-Report-Data" target="_blank"><img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></li>
 	    </ul>
 	  </footer>
